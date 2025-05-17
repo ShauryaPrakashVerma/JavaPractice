f7ada4dff4a4b1c47f40d79cef7e15928a898922
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-05-17 13:34:32 +0530

    Update Pattern4.java

--- Projects/Pattern4.java
+++ Projects/Pattern4.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-public class Pattern1 {
+public class Pattern4{
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter row and col">);
+		System.out.println("Enter row and col:");
 		int row = sc.nextInt();
 		int col = sc.nextInt();
 		for (int i = 1; i <=row; i++) {
