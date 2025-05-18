5827d156732ed62959da92f6c6644ec3f377c3b2
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-05-18 17:48:24 +0000

    To transpose a Matrix

--- Projects/Transpose.java
+++ Projects/Transpose.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
-public class Solution {
+public class Transpose {
     public static void main(String[] args) {
+        // Initializing a 2D array with values
         int[][] twodm = {
                 {10, 20, 30},
                 {40, 50, 60}
         };
+        
         System.out.print("Original Array:\n");
         print_array(twodm);
         
-        System.out.print("After changing the rows and columns of the said array:");
+        System.out.println("After changing the rows and columns of the said array:");
         transpose(twodm);
     }
+    
     private static void transpose(int[][] twodm) {
-
+     
         int[][] newtwodm = new int[twodm[0].length][twodm.length];
-        
         for (int i = 0; i < twodm.length; i++) {
             for (int j = 0; j < twodm[0].length; j++) {
                 newtwodm[j][i] = twodm[i][j];
             }
         }
-       
+        
         print_array(newtwodm);
     }
     private static void print_array(int[][] twodm) {
