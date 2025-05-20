046afb65fb9812e194c447b9fddc1e9e051bd0b4
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-05-20 14:39:02 +0000

    Program to check the occurence of an element in an array

--- Projects/NumberOccur.java
+++ Projects/NumberOccur.java
@@ -5,7 +5,7 @@ public class NumberOccur{
         Scanner sc=new Scanner(System.in);
         int[] arr=new int[5];
         for(int i=0; i<5; i++){
-            System.out.println("Enter array element at " + i +":");
+            System.out.print("Enter array element at " + i +":");
             arr[i]=sc.nextInt();
         }
 
