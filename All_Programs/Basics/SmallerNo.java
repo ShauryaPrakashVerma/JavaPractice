fc8bf492c5f53c0a1a5dd6abfcca2d7d0c2be14f
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-05-30 18:25:05 +0000

    Program to check smaller number through ternary operator

---
+++ All_Programs/Basics/SmallerNo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SmallerNo{
+    public static void main(String...args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int num1=sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2=sc.nextInt();
+
+        SmallerNo small=new SmallerNo();
+        int min= small.min(num1, num2);
+        System.out.println("")
+    }
+
+    public int min(int num1, int num2){
+        return num1>num2? num2: num1;
+    }
+}
