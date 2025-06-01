d3930a238e1c7ae4a16fcac41e70ee89662f9024
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-06-01 13:58:42 +0000

    Program to calculate area and circumference of a class

---
+++ All_Programs/Basics/Circle.java
@@ -0,0 +1,32 @@
+//Calculate the area and circumference of a circle for a given radius using Math.PI
+import java.util.*;
+class Circle{
+    double radius;
+    
+
+    Circle(double radius){
+        this.radius=radius;
+    }
+
+    double getCircumference(){
+        return 2*radius*Math.PI;
+    }
+
+    double getArea(){
+        return Math.PI*Math.pow(radius,2);
+    }
+    
+    public String toString(){
+        return "Circle Props:\nRadius: "+radius
+                        +",\nCircumference: "+getCircumference()
+                        +",\nArea: "+getArea();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the radius");
+        double radius=sc.nextDouble();
+        Circle circle=new Circle(radius);
+        System.out.println(circle);
+    }
+}
