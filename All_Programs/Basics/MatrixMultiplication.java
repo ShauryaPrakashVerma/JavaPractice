dae3075a32c0f9f25df9fb63097e687062ff5d7f
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-06-06 18:30:34 +0000

    Program to multiply two matrices and print the output.

--- All_Programs/Basics/MatrixMultiplication.java
+++ All_Programs/Basics/MatrixMultiplication.java
@@ -56,12 +56,11 @@ class MatrixMultiplication
     }
     res[i][j]=sum;
     }
-    for ( i= 0 ; i < r1; i++ )
+    for (i= 0 ;i < r1; i++)
     {
-    for ( j=0 ; j < c2;j++ )
-    System.out.print(res[i][j]+" ");
-
-    System.out.println();
+        for ( j=0 ; j < c2;j++ )
+        System.out.print(res[i][j]+" ");
+        System.out.println();
     }
     }
     else
