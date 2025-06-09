6f8f4abb535a461bf42913c94c4135e6b3353e33
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-06-09 14:09:07 +0000

    updated

---
+++ All_Programs/com/example/utils/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.utils;
+
+import com.example.geometry.Circle;
+import com.example.geometry.Rectangle;
+
+public class Calculator{
+    public static void main(String[] args) {
+        Circle car =new Circle(5.5);
+        Rectangle rect=new Rectangle(10,5);
+    }
+}
