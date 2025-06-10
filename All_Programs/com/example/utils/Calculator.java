8bc2b6eae11ce3db31a6d4c7b850ec0a4980e0d9
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-06-10 10:47:00 +0000

    Program number 77 still not running.

--- All_Programs/com/example/utils/Calculator.java
+++ All_Programs/com/example/utils/Calculator.java
@@ -5,7 +5,13 @@ import com.example.geometry.Rectangle;
 
 public class Calculator{
     public static void main(String[] args) {
-        Circle car =new Circle(5.5);
+        Circle cir =new Circle(5.5);
         Rectangle rect=new Rectangle(10,5);
+
+        double circArea= Math.PI * Math.pow(cir.radius,2);
+        double rectArea= rect.length*rect.breadth;
+
+        System.out.println("Area of the circle is "+circArea);
+        System.out.println("Area of the rectangle is "+rectArea);
     }
 }
