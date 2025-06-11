71ba7d9f1624d3b47e356f9011919e7518df9e82
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2025-06-11 14:36:00 +0000

    Folder organization changed.

--- All_Programs/OOPS/Challenge78/Customer/customer.java
+++ All_Programs/OOPS/Challenge78/Customer/customer.java
@@ -1,10 +1,10 @@
 package Challenge78.Bank;
 
-// import Challenge78.Bank.BankAccount;
+import Challenge78.Bank.BankAccount;
 
 class customer{
     public static void main(String[] args) {
-        BankAccount account=new BankAccount("001","Shaurya Prakash");
+        BankAccount account = new BankAccount("001","Shaurya Prakash");
         account.depositMoney(100);
         System.out.println(account.withdrawMoney(200));
         account.depositMoney(-200);
